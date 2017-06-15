ac3bef34fea08b150175473dda63d131f6451a75
JTong <eldertong@gmail.com>
2017-06-15 16:44:44 -0400

    somewhat working

--- alert.js
+++ alert.js
@@ -1,19 +1,37 @@
-// chrome.tabs.getSelected(null, function(tab) {
-//     myFunction(tab.url);
-// });
+  $(function() {
+      //   $('#onButton').bind('click', function() {
+      //       $("#onButton").hide();
+      //       $("#offButton").show();
+      //   });
+      //   $('#offButton').bind('click', function() {
+      //       $("#offButton").hide();
+      //       $("#onButton").show();
+      //   });
+      chrome.tabs.getSelected(null, function(tab) {
+          myFunction(tab.url);
+      });
+  });
 
-// function myFunction(tablink) {
-//     var site, userName, userID;
-//     site = String(tablink);
-//     tablink = String(tablink);
-//     console.log(tablink, "tablink")
-//     if (site.includes("facebook")) {
-//         alert("Gramps, you are on Facebook.  Do not embarrass me!");
+  function myFunction(tablink) {
+      var site, userName, userID;
+      site = String(tablink);
+      tablink = String(tablink);
+      console.log(tablink, "tablink")
+      if (site.includes("facebook")) {
+          alert("Gramps, you are on Facebook.  Do not embarrass me!");
 
-//     } else if (site.includes("google")) {
-//         alert("This is your search page, feel free to search whatever you want.")
-//     } else {
-//         alert("You are not on facebook or google.")
-//     }
-// }
-// console.log(tablink);
+      } else if (site.includes("google")) {
+          alert("This is your search page, feel free to search whatever you want.")
+      } else {
+          alert("You are not on facebook or google.")
+      }
+  }
+  console.log(tablink);
+  // $(document).ready(function() {
+  //     $("#hide").click(function() {
+  //         $("p").hide();
+  //     });
+  //     $("#show").click(function() {
+  //         $("p").show();
+  //     });
+  // });
