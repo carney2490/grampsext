ccf08d3502df169d9fe8ceab5d149d6d8d04c326
JTong <eldertong@gmail.com>
2017-06-15 15:23:08 -0400

    trying to get alerts in the right place

--- background.js
+++ background.js
@@ -1,16 +1,33 @@
-function whatSiteIsGrampsOn(tablink){
+//   function whatSiteIsGrampsOn(tablink) {
+//       var site, userName, userID;
+//       site = String(tablink);
+//       tablink = String(tablink);
+//       console.log(tablink, "tablink")
+//       if (site.includes("facebook")) {
+//           alert("Gramps, you are on Facebook.  Do not embarrass me!");
+
+//       } else if (site.includes("google")) {
+//           alert("This is your search page, feel free to search whatever you want.")
+//       } else {
+//           alert("You are not on facebook or google.")
+//       }
+//   }
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+
         var site, userName, userID;
-    site = String(tablink);
-    tablink = String(tablink);
-    console.log(tablink, "tablink")
-if (site.includes("facebook")) {
-        alert("Gramps, you are on Facebook.  Do not embarase me!");
-    
-}
-else if (site.includes("google")){
-        alert("This is your search page, feel free to search whatever you want.")
-}
-else {
-        alert("You are not on facebook or google.")
-}
-}
+        site = String(tablink);
+        tablink = String(tablink);
+        console.log(tablink, "tablink")
+        if (site.includes("facebook")) {
+            alert("Gramps, you are on Facebook.  Do not embarrass me!");
+
+        } else if (site.includes("google")) {
+            alert("This is your search page, feel free to search whatever you want.")
+        } else {
+            alert("You are not on facebook or google.")
+        }
+
+    }
+})
