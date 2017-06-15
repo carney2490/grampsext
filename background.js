eed7d9d17681a04355e034a8920f2cacc7101cc2
JTong <eldertong@gmail.com>
2017-06-15 16:36:02 -0400

    still trying to get the code up and running

--- background.js
+++ background.js
@@ -1,33 +1,66 @@
-//   function whatSiteIsGrampsOn(tablink) {
-//       var site, userName, userID;
-//       site = String(tablink);
-//       tablink = String(tablink);
-//       console.log(tablink, "tablink")
-//       if (site.includes("facebook")) {
-//           alert("Gramps, you are on Facebook.  Do not embarrass me!");
+chrome.tabs.getSelected(null, function(tab) {
+    myFunction(tab.url);
+});
 
-//       } else if (site.includes("google")) {
-//           alert("This is your search page, feel free to search whatever you want.")
-//       } else {
-//           alert("You are not on facebook or google.")
-//       }
-//   }
+function myFunction(tablink) {
+    var site, userName, userID;
+    site = String(tablink);
+    tablink = String(tablink);
+    console.log(tablink, "tablink")
+    if (site.includes("facebook")) {
+        alert("Gramps, you are on Facebook.  Do not embarrass me!");
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
-
-        var site, userName, userID;
-        site = String(tablink);
-        tablink = String(tablink);
-        console.log(tablink, "tablink")
-        if (site.includes("facebook")) {
-            alert("Gramps, you are on Facebook.  Do not embarrass me!");
-
-        } else if (site.includes("google")) {
-            alert("This is your search page, feel free to search whatever you want.")
-        } else {
-            alert("You are not on facebook or google.")
-        }
-
+    } else if (site.includes("google")) {
+        alert("This is your search page, feel free to search whatever you want.")
+    } else {
+        alert("You are not on facebook or google.")
     }
-})
+}
+console.log(tablink);
+
+
+// function whatSiteIsGrampsOn(tablink) {
+//     var site, userName, userID;
+//     site = String(tablink);
+//     tablink = String(tablink);
+//     console.log(tablink, "tablink")
+//     if (site.includes("facebook")) {
+//         alert("Gramps, you are on Facebook.  Do not embarrass me!");
+
+//     } else if (site.includes("google")) {
+//         alert("This is your search page, feel free to search whatever you want.")
+//     } else {
+//         alert("You are not on facebook or google.")
+//     }
+// }
+
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//     if (changeInfo.status == 'complete') {
+
+//         var site, userName, userID;
+//         site = String(tablink);
+//         tablink = String(tablink);
+//         console.log(tablink, "tablink")
+//         if (site.includes("facebook")) {
+//             alert("Gramps, you are on Facebook.  Do not embarrass me!");
+
+//         } else if (site.includes("google")) {
+//             alert("This is your search page, feel free to search whatever you want.")
+//         } else {
+//             alert("You are not on facebook or google.")
+//         }
+
+//     }
+// })
+
+// chrome.notifications.create(
+//     'name-for-notification', {
+//         type: 'basic',
+//         iconUrl: 'image.jpeg',
+//         title: "This is a notification",
+//         message: "hello there!"
+//     },
+
+//     function() {}
+
+// );
