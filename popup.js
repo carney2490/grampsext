a0686f69762094f0c6c2327dbd932c1c31426267
JTong <eldertong@gmail.com>
2017-06-15 12:24:09 -0400

    working on getting on and off to toggle

--- popup.js
+++ popup.js
@@ -1,41 +1,27 @@
-/**
- * Add your Analytics tracking ID here.
- */
-var _AnalyticsCode = 'UA-100785529-1';
-/**
- * Below is a modified version of the Google Analytics asynchronous tracking
- * code snippet.  It has been modified to pull the HTTPS version of ga.js
- * instead of the default HTTP version.  It is recommended that you use this
- * snippet instead of the standard tracking snippet provided when setting up
- * a Google Analytics account.
- */
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', _AnalyticsCode]);
-_gaq.push(['_trackPageview']);
-(function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
-/**
- * Track a click on a button using the asynchronous tracking API.
- *
- * See http://code.google.com/apis/analytics/docs/tracking/asyncTracking.html
- * for information on how to use the asynchronous tracking API.
- */
-function trackButtonClick(e) {
-    _gaq.push(['_trackEvent', e.target.id, 'clicked']);
-}
-/**
- * Now set up your event handlers for the popup's `button` elements once the
- * popup's DOM has loaded.
- */
-document.addEventListener('DOMContentLoaded', function() {
-    var buttons = document.querySelectorAll('button');
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', trackButtonClick);
-    }
+$(function() {
+    $('#onButton').bind('click', function() {
+
+        chrome.extension.getBackgroundPage().setSnoozeUntil(new Date(new Date().getTime() + 20000).getTime());
+        $("#onButton").hide();
+        $("#offButton").show();
+        // self.close();
+
+    });
+    $('#stopButton').bind('click', function() {
+        chrome.extension.getBackgroundPage().setSnoozeUntil(null);
+        $("#offButton").hide();
+        $("#onButton").show();
+        // self.close();
+    });
+
+    // $('button').hide();
+
+
+    chrome.storage.local.get(function(settings) {
+        $('#recruitModeButton').toggle(!settings.recruitMode);
+        $('#normalButton').toggle(settings.recruitMode);
+        var isSnoozing = !(!settings.snoozeUntil || settings.snoozeUntil < new Date().getTime);
+        $('#onButton').toggle(!isSnoozing);
+        $('#offButton').toggle(isSnoozing);
+    });
 });
